a369aea864567e7e52ad40c45515a7b28538d64a
Jason Yang <jasony1@andrew.cmu.edu>
2019-01-04 20:12:18 -0500

    reorg

---
+++ Charles1/PythonReader/BufferedWriter.py
@@ -0,0 +1,63 @@
+import threading
+import queue
+import time
+import os
+
+class BufferedWriter(threading.Thread):
+
+	def __init__(self, dataBuffer, filename):
+
+		threading.Thread.__init__(self);
+
+
+		if not os.path.exists("output/"):
+			os.makedirs("output/")
+
+		self._buffer = dataBuffer;
+		self._filename = filename;
+		self._isAlive = True;
+
+	def run(self):
+		print("Starting Writer");
+
+		if(self._filename == None):
+			while(self._isAlive):
+				try:
+					data = self._buffer.get(block=True, timeout=1);
+					# data = data[::2];
+					# file.write(data);
+					# file.write(self._buffer.get(block=True, timeout=1));
+				except queue.Empty:
+					continue;
+				except Exception as e:
+					print(e);
+					break;
+
+		else:
+			with open('output/' + self._filename, 'wb') as file:
+				while(self._isAlive):
+					try:
+						data = self._buffer.get(block=True, timeout=1);
+					#	data = data[::2];
+						file.write(data);
+						# file.write(self._buffer.get(block=True, timeout=1));
+					except queue.Empty:
+						continue;
+					except Exception as e:
+						print(e);
+						break;
+
+				print("Flushing Buffer");
+				count = self._buffer.qsize();
+				try:
+					for i in range(count):
+						file.write(self._buffer.get(block=True, timeout=1));
+				except queue.Empty:
+					print("Error flushing buffer... aborting");
+
+		print("Writer Closed");
+
+
+	def shutdown(self):
+		self._isAlive = False;
+		self.join();
--- PythonReader/tester.py
+++
@@ -1,5 +0,0 @@
-import G2Display
-
-test = G2Display.G2GraphWindow(None);
-
-
