084fcd4ba614f890be143f80c6d05ab95d969c56
Jason Yang <jasony1@andrew.cmu.edu>
2019-01-12 22:42:01 -0500

    removed tracking

--- MatlabExtractor2/g1diff.m
+++
@@ -1,9 +0,0 @@
-function [dif] = g1diff(aDbr0,  beta,  g2, snr, tau, rho , no, k0, mua, musp)
-    g1an = g1analytical(aDbr0, tau', rho , no, k0, mua, musp);
-    
-    G1 = sqrt(abs((g2-1)./beta));
-    G1 = G1';
-    
-    snr = snr.*(snr>1);
-    dif = sum(((G1 - g1an).* snr' ).^2);
-end
