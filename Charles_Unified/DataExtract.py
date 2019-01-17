a978dfca81b9a09501258564cbc9215dec7b1dc5
Jason Yang <jasony1@andrew.cmu.edu>
2019-01-17 17:05:37 -0500

    final extract update

--- Charles_Unified/DataExtract.py
+++ Charles_Unified/DataExtract.py
@@ -36,6 +36,15 @@ def batchFullExtract(files, averages, legacy=False, fs=2.5E6, intg=0.05, fsout=2
 	for f in files:
 		fullExtract(f, averages, legacy, fs, intg, fsout, rho, no, wavelength, mua, musp, numProcessors)
 
+def csv2Matlab(filename, averages, legacy=False, fs=2.5E6, intg=0.05, fsout=200, rho=2, no=1.33, wavelength=8.48E-5, mua=0.1, musp=10, numProcessors=6):
+	folder = G2Extract.createFolder(filename, intg);
+	(g,t,v) = G2Extract.loadG2(folder);
+	filename = G2Extract.writeG2Matlab(filename, g, t, v, legacy, fs, intg, fsout);
+	print("Completed G2");
+	(flows, betas, counts, g2a) = FlowExtract.calculateFlow(g, t, averages, fs, rho, no, wavelength, mua, musp, numProcessors);
+	FlowExtract.writeFlowMatlab(filename, flows, betas, counts, g2a, averages, rho, no, wavelength, mua, musp);
+	print("Completed Flow");
+
 def fullExtractMatlab(filename, averages, legacy=False, fs=2.5E6, intg=0.05, fsout=200, rho=2, no=1.33, wavelength=8.48E-5, mua=0.1, musp=10, numProcessors=6):
 	print("Extracting: " + filename);
 	(g, t, v) = G2Extract.calculateG2(filename, legacy, fs, intg, fsout, numProcessors);
