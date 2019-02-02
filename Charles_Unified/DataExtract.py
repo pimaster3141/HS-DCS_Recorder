c71459dddd930d8dd1f4753dd999465da18ee7b1
Jason Yang <jasony1@andrew.cmu.edu>
2019-02-01 22:38:44 -0500

    .\DataExtract.py

--- Charles_Unified/DataExtract.py
+++ Charles_Unified/DataExtract.py
@@ -60,6 +60,7 @@ def csv2Matlab(filename, averages, legacy=False, fs=2.5E6, intg=0.05, fsout=200,
 def fullExtractMatlab(filename, averages, legacy=False, fs=2.5E6, intg=0.05, fsout=200, rho=2, no=1.33, wavelength=8.48E-5, mua=0.1, musp=10, numProcessors=6):
 	print("Extracting: " + filename);
 	(g, t, v) = G2Extract.calculateG2(filename, legacy, fs, intg, fsout, numProcessors);
+	print(len(t));
 	filename = G2Extract.writeG2Matlab(filename, g, t, v, legacy, fs, intg, fsout);
 	print("Completed G2");
 	(flows, betas, counts, g2a) = FlowExtract.calculateFlow(g, t, averages, fs, rho, no, wavelength, mua, musp, numProcessors);
