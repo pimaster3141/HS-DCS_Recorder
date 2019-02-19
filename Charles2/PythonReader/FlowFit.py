b4f7658401552136d74f54274db70a01642af26e
Jason Yang <jasony1@andrew.cmu.edu>
2019-02-19 12:47:34 -0500

    rebuild -delete

--- Charles2/PythonReader/FlowFit.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-class G1Converter:
-	def calcBeta(g2Data, limit=5):
-		return np.mean(g2Data[1:limit]);
-
-	def G1Calc(g2Data):
-		beta = calcBeta(g2Data);
-		g1Data = np.sqrt(np.abs((g2Data-1)/beta));
