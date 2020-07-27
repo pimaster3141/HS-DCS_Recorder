9d629ea1b6afb4ad03c70f64eb102e1f4094b48e
Jason Yang <jasony1@andrew.cmu.edu>
2020-07-27 04:41:36 -0400

    update tolerances in fit

--- Charles_Unified/PythonLib/FlowFit.pyx
+++ Charles_Unified/PythonLib/FlowFit.pyx
@@ -119,7 +119,7 @@ def G2Fit(g2Data, tauList, SNR, p0=[1E-9, 0.25], rho=2, no=1.33, wavelength=8.48
 		return G2Analytical(aDb, beta, tau, rho, no, wavelength, mua, musp)*SNR;
 
 	try:
-		(params, params_covariance) = optimize.curve_fit(f, tauList, g2Data*SNR, p0, bounds=((aDb_BOUNDS[0], BETA_BOUNDS[0]), (aDb_BOUNDS[1], BETA_BOUNDS[1])), ftol=None, xtol=1E-10, gtol=None);
+		(params, params_covariance) = optimize.curve_fit(f, tauList, g2Data*SNR, p0, bounds=((aDb_BOUNDS[0], BETA_BOUNDS[0]), (aDb_BOUNDS[1], BETA_BOUNDS[1])), ftol=None, gtol=None);
 		return params;
 	except:
 		# print("fit Error:");
