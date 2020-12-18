ee2bdc6e42b9d414f72a21f58870b300926e465e
Jason Yang <pimaster3141@gmail.com>
2020-12-18 15:26:40 -0500

    updated g2 save flags

--- Charles_Unified/DataExtract.py
+++ Charles_Unified/DataExtract.py
@@ -79,7 +79,7 @@ print("Done");
 # 	FlowExtract.writeFlowMatlab(filename, flows, betas, counts, g2a, averages, rho, no, wavelength, mua, musp);
 # 	print("Completed Flow");
 
-def fullExtractMatlab(filename, averages, legacy=None, fs=2.5E6, intg=0.05, fsout=200, levels=16, rho=2, no=1.33, wavelength=8.48E-5, mua=0.1, musp=10, numProcessors=None, saveG2=False):
+def fullExtractMatlab(filename, averages, legacy=None, fs=2.5E6, intg=0.05, fsout=200, levels=16, rho=2, no=1.33, wavelength=8.48E-5, mua=0.1, musp=10, numProcessors=None, saveG2=False, saveG2Average=False):
 	if(numProcessors==None):
 		numProcessors = psutil.cpu_count(logical=False);
 		print("Autoselecting Core Count: " + str(numProcessors));
@@ -102,7 +102,7 @@ def fullExtractMatlab(filename, averages, legacy=None, fs=2.5E6, intg=0.05, fsou
 	filename = G2Extract.writeG2Matlab(filename, g, t, v, legacy, fs, intg, fsout, saveG2);
 	print("Completed G2");
 	(flows, betas, counts, g2a) = FlowExtract.calculateFlow(g, t, averages, fs, rho, no, wavelength, mua, musp, numProcessors);
-	FlowExtract.writeFlowMatlab(filename, flows, betas, counts, g2a, averages, rho, no, wavelength, mua, musp, saveG2);
+	FlowExtract.writeFlowMatlab(filename, flows, betas, counts, g2a, averages, rho, no, wavelength, mua, musp, saveG2Average);
 	print("Completed Flow");
 
 	del(g);
